797c557165d1f6d7878c82084b89cd39954bcf19
Noel Arlante <noelitoarlante@gmail.com>
2012-09-19 17:16:29 +0800

    Added Store and Tablet class

---
+++ GDGT.eQuote.Model/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDGT.eQuote.Model
+{
+    public class Store
+    {
+        public int StoreId { get; set; }
+        public string Name { get; set; }
+        public List<ProductBase> Products { get; set; }
+
+    }
+}
---
+++ GDGT.eQuote.Model/Tablet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GDGT.eQuote.Model
+{
+    public class Tablet : ProductBase
+    {
+        public double ScreenSize { get; set; }
+        public int ScreenResolution { get; set; }
+        public bool FrontFaceCamera { get; set; }
+        public bool RearCamera { get; set; }
+
+
+
+        
+    }
+}
